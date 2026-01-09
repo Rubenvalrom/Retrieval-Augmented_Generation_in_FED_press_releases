b60ad649b81cdb50ffdbee29e52ce788bd173930
RubenVR <ruben627vr@gmail.com>
2026-01-09 12:37:39 +0100

    Add spaCy-based lemmatization and stopword removal
    Introduced a new preprocessing step in clean_data.py to remove stop words and lemmatize document content using spaCy, while preserving negative words. Updated environment.yml and requirements.txt to include spaCy and its dependencies.

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader 
 import os
-from urllib.parse import urljoin
 import joblib
+import spacy
 
 INPUT_DIR = "raw"
 OUTPUT_DIR = "clean"
@@ -37,6 +37,22 @@ def remove_first_last_line(raw_document):
     raw_document.page_content = new_text
     return raw_document
 
+def filter_lemmatize(raw_document, nlp): # Pass the whole object
+    """
+    Remove stop words and lemmatize, then update the document content.
+    """
+    # Process the text content
+    spacy_doc = nlp(raw_document.page_content)
+
+    # Filter and lemmatize  
+    filtered_tokens = [token for token in spacy_doc if not token.is_stop]
+    lemmatized_text = " ".join([token.lemma_.lower().strip() for token in filtered_tokens])
+    
+    # Update the object's content and return the whole object
+    raw_document.page_content = lemmatized_text
+    return raw_document
+
+
 # Clean metadata to keep only relevant fields
 def clean_metadata(raw_document, metadata_to_keep=METADATA_TO_KEEP):
     """
@@ -50,11 +66,30 @@ def clean_data(output_dir=OUTPUT_DIR, input_dir=INPUT_DIR):
     # Execute the data cleaning process
     setup_directory(output_dir)
     raw_documents = download_pdfs(input_dir)
-    print(f"Total number of pages loaded: {len(raw_documents)} páginas de PDF en total.")
+    print(f"Total number of pages loaded: {len(raw_documents)}")
 
+    # Remove first and last lines from each document
     cutted_documents = [remove_first_last_line(doc) for doc in raw_documents]
-    clean_documents = [clean_metadata(doc) for doc in cutted_documents]
+    print("First and last lines removed from each document.")
 
+    # Load spaCy model (just the tokenizer and lemmatizer)
+    nlp = spacy.load("en_core_web_sm", disable=["parser", "ner", "textcat"])
+    print("spaCy model loaded.")
+
+    # Keep negative words
+    negative_words = ["no", "not", "nor", "n't", "never", "none", "nobody", "nothing", "neither", "without", "nevertheless"]
+
+    for word in negative_words:
+        nlp.vocab[word].is_stop = False
+
+    # Remove stop words and lemmatize
+    root_documents = [filter_lemmatize(doc, nlp) for doc in cutted_documents]
+    print("Stop words removed and lemmatization applied.")
+
+    # Clean metadata
+    clean_documents = [clean_metadata(doc) for doc in root_documents]
+    print("Metadata cleaned.")
+    
     # Save cleaned documents in a pickle file
     joblib.dump(clean_documents, os.path.join(output_dir, 'clean_documents.pkl'))
 
